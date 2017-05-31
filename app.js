bdd516a41d713ba432ed55fa8e195c646b2addd3
dagneserodrigo <dagnese.rodrigo@gmail.com>
2017-05-31 14:23:09 -0300

    fix(package.json) adicionado dependência express-load

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var express = require('express')
-var load = require('express-load')
-var path = require('path')
-var bodyParser = require('body-parser')
+var express = require('express');
+var load = require('express-load');
+var path = require('path');
+var bodyParser = require('body-parser');
 //import socketIo from 'socket.io'
-var http = require('http')
-var config = require('./config/config')
+var http = require('http');
+var config = require('./config/config');
 
-var app = express()
+var app = express();
 
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -15,15 +15,15 @@ var app = express()
 //   next()
 // })
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 load('models')
   .then('controllers')
   .then('routes')
   .into(app)
 
-const server = http.createServer(app)
+const server = http.createServer(app);
 //const io = socketIo.listen(server)
 
 // load('sockets')
@@ -32,5 +32,5 @@ const server = http.createServer(app)
 var port = process.env.PORT || config.PORT;
 
 server.listen(port, function(){
-    console.log('Listening in port ' + port)
+    console.log('Listening in port ' + port);
 });
